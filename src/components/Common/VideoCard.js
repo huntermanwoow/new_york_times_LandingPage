d4af36e20b8d53aef627388f79adba70f728eb0c
khanzohan <zoho12@live.com>
2024-06-20 22:13:51 -0400

    update the token price

--- src/components/Common/VideoCard.js
+++ src/components/Common/VideoCard.js
@@ -10,7 +10,7 @@ export default function VideoCard () {
                 <iframe className="w-full h-[320px] md:h-[384px] rounded-[20px]" src="https://www.youtube.com/@RAD1913" allowFullScreen></iframe>
             </div>
             <div className="flex mt-4 justify-between">
-                <PriceCard icon={ChartIcon.src} price={'$31.55'} description={'Guranteed floor price'} />
+                <PriceCard icon={ChartIcon.src} price={'$31.72'} description={'Guranteed floor price'} />
                 <PriceCard icon={CommandIcon.src} price={'$31332.55'} description={'Reserved money'} />
             </div>
         </>
